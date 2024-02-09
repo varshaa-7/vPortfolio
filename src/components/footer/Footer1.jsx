1ab5c5bf37ab9bafa6a7e7b4d1202e9ec0b6648b
Madhur Gaur <madhurgaur9140@gmail.com>
2024-02-09 22:32:04 +0530

    aa

---
+++ src/components/footer/Footer1.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+
+const Footer1 = () => {
+  return (
+    <footer className="bg-violet-100 border-y">
+    <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+            <div className="mb-6 md:mb-0">
+                <Link to="/" className="flex items-center">
+                    <img
+                        src="https://i.ibb.co/xCR1BXR/p-removebg-preview.png"
+                        className="mr-3 h-16"
+                        alt="Logo"
+                    />
+                </Link>
+            </div>
+            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                <div>
+                    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+                    <ul className="text-gray-700 font-medium">
+                        <li className="mb-4">
+                            <Link to="/" className="before:text-gray-700 hover:text-purple-700">
+                                Home
+                            </Link>
+                        </li>
+                        <li className=" ">
+                            <Link to="/about" className="before:text-gray-700 hover:text-purple-700">
+                                About
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow me</h2>
+                    <ul className="text-gray-700 font-medium">
+                        <li className="mb-4">
+                            <a
+                                href="https://github.com/varshaa-7"
+                                className="before:text-gray-700 hover:text-purple-700"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                Github
+                            </a>
+                        </li>
+                        <li>
+                        <a
+                                href="https://www.linkedin.com/in/varsha-awasthi-1a7a31257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                                className="before:text-gray-700 hover:text-purple-700"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                LinkedIn
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
+                    <ul className="text-gray-700 font-medium">
+                        <li className="mb-4">
+                            <Link to="#" className="before:text-gray-700 hover:text-purple-700">
+                                Privacy Policy
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="before:text-gray-700 hover:text-purple-700">
+                                Terms &amp; Conditions
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <span className="text-sm text-gray-500 sm:text-center">
+                © 2024
+                <a href="https://hiteshchoudhary.com/" className="hover:underline">
+                    varshaawasthi
+                </a>
+                . All Rights Reserved.
+            </span>
+            <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+                
+                
+                <Link to="https://www.linkedin.com/in/varsha-awasthi-1a7a31257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="text-gray-500 hover:text-gray-900">
+                    <svg
+                        className="w-4 h-4"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M3 0h18a3 3 0 0 1 3 3v18a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V3a3 3 0 0 1 3-3zm0 5.11A2.23 2.23 0 0 0 .77 7a2.27 2.27 0 0 0 0 3.19 2.17 2.17 0 0 0 3.06.08 2.19 2.19 0 0 0 .08-3.06zM5 5.47a7.92 7.92 0 0 1 .33 2.28v13.54a7.85 7.85 0 0 1-1.83 5.05 8.07 8.07 0 0 1-5.44 2.32H0V18.39a10.77 10.77 0 0 1 .28-2.44 4.18 4.18 0 0 1 1.35-2 3.76 3.76 0 0 1 2.51-.89 4.24 4.24 0 0 1 2.63 1.07 6.68 6.68 0 0 1 .77.67v-.64H9.41v-6.1h3.18v6.1h1.87v-6.1h3.17v6.1h1.87v-6.9a6.32 6.32 0 0 1 .11-1.25h2.71z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                    <span className="sr-only">LinkedIn Account</span>
+                </Link>
+                <Link to="https://github.com/varshaa-7" className="text-gray-500 hover:text-gray-900">
+                    <svg
+                        className="w-4 h-4"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                    <span className="sr-only">GitHub account</span>
+                </Link>
+                <Link to="awasthivarsha0710@gmail.com" className="text-gray-500 hover:text-gray-900">
+                    <svg
+                        className="w-4 h-4"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                    <span className="sr-only">Gmail account</span>
+                </Link>
+            </div>
+        </div>
+    </div>
+</footer>
+  )
+}
+
+export default Footer1
