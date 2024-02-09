c52ff978fd0383bce0a809492a98f124b4874605
Madhur Gaur <madhurgaur9140@gmail.com>
2024-02-09 22:35:32 +0530

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 
 import './App.css'
 import Footer1 from './components/footer/Footer1'
-import Header from './components/header/Header1'
+import Header1 from './components/header/Header1'
 import Home from './components/home/Home'
 
 function App() {
   
   return (
     <>
-    <Header/>
+    <Header1/>
     <Home/>
     <Footer1/>
 
